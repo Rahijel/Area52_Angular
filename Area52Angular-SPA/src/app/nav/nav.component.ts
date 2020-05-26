51077b9d58034ca1ba7afbe0e88ba89e50a11629
AzureAD\ManuelKrause <Manuel.Krause@lorent-online.com>
2020-05-26 21:06:43 +0200

    ErrorHandling

--- Area52Angular-SPA/src/app/nav/nav.component.ts
+++ Area52Angular-SPA/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Login funzt');
     }, error => {
-      console.log('Wasted');
+      console.log(error);
     });
   }
 
